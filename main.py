b4fd5d040ac07fdb20a512060b9ac50b4c18fff6
Ibrahim Ouezghar <ibrahim.ouezghar@gmail.com>
2026-05-28 21:53:23 +0200

    main update

--- main.py
+++ main.py
@@ -2,5 +2,5 @@ from utils.openspace import Openspace
 from utils.table import Table
 from utils.table import Seat
 
-file = open("utils/colleagues.txt", "r")
+file = open("utils/new_colleagues.txt", "r")
 print(file)
